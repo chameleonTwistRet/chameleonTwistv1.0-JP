4f85254aaa6726efd93958dafe00a365de7dcb5f
Ellie <EllieRHudson@gmail.com>
2023-04-01 20:54:01 +0100

    2 reverb funcs

--- src/audio/reverb.c
+++ src/audio/reverb.c
@@ -1,9 +1,47 @@
 #include "common.h"
 
 #include <libaudio.h>
+#include <ultraerror.h>
 #include "PR/synthInternals.h"
+#include <os.h>
+#include <os_internal.h>
+#include <assert.h>
+#include "PR/initfx.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/audio/reverb/func_800ED3E0.s")
+#define RANGE 2.0
+
+#define SWAP(in, out)	\
+{			\
+     s16 t = out;	\
+     out = in;		\
+     in = t;		\
+}
+
+/***********************************************************************
+ * Reverb filter public interfaces
+ ***********************************************************************/
+
+/*
+ * Generate a triangle wave from -1 to 1, and find the current position
+ * in the wave. (Rate of the wave is controlled by d->rsinc, which is chorus
+ * rate) Multiply the current triangle wave value by d->rsgain, (chorus depth)
+ * which is expressed in number of samples back from output pointer the chorus
+ * should go at it's full chorus. In otherwords, this function returns a number
+ * of samples the output pointer should modulate backwards.
+ */
+f32 _doModFunc(ALDelay* d, s32 count) {
+    f32 val;
+
+    d->rsval += d->rsinc * count;
+    d->rsval = (d->rsval > RANGE) ? d->rsval-(RANGE*2) : d->rsval;
+    
+    val = d->rsval;
+    val = (val < 0) ? -val : val;
+    
+    val -= RANGE/2;
+
+    return(d->rsgain * val);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/audio/reverb/func_800ED488.s")
 
@@ -15,7 +53,6 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/audio/reverb/alFxParamHdl.s")
 
-//#pragma GLOBAL_ASM("asm/nonmatchings/audio/reverb/alFxParam.s")
 s32 alFxParam(void *filter, s32 paramID, void *param)
 {
     if(paramID == AL_FILTER_SET_SOURCE)
